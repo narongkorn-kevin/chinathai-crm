e8ac31edfc327452dfbaf3176b07fb4eefa0cc5e
narongkorn-kevin <narongkorn.kevin@gmail.com>
2025-07-28 12:01:28 +0700

    change env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://cargo-api.dev-asha9.com'
-    // apiUrl: 'https://sodexo-upos.ddns.net:3000'
-    // apiUrl: 'http://localhost:3000'
+    apiUrl: 'https://g-cargo.dev-asha9.com'
   };
