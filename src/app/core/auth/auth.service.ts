3e670d17d7cd7350aafd676a6abef3748804b6be
narongkorn-kevin <narongkorn.kevin@gmail.com>
2025-07-30 20:43:52 +0700

    update layout

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -72,7 +72,7 @@ export class AuthService
                 
                 // Store the access token in the local storage
                 this.accessToken = response.token;
-                localStorage.setItem('lang','en');
+                localStorage.setItem('lang','th');
                 localStorage.setItem('user', JSON.stringify(response.data));
                 // Set the authenticated flag to true
                 this._authenticated = true;
